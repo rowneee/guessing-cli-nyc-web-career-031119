220573cecd217ca93f5e85e631a2f33df3cfbd23
Roni Shabo <roni.shabo@gmail.com>
2019-03-05 20:29:52 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -4,9 +4,9 @@ def run_guessing_game
   number = rand(1..6)
     while user_response != "exit" do
       if user_response.to_i == number
-        "You guessed the correct number!" 
+        puts "You guessed the correct number!" 
       else  
-        "The computer guessed #{number}"
+        puts "The computer guessed #{number}"
       end
       user_response = gets.chomp
       number = rand(1..6)
