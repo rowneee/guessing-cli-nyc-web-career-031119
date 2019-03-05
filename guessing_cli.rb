09da567c095cb3cac75d8901e86c3465cf918364
Roni Shabo <roni.shabo@gmail.com>
2019-03-05 20:15:21 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,5 +2,8 @@ def run_guessing_game
   user_input == gets.chomp
   number = Rand(1..6)
   if user_input == number
-    
+    return "You guess the correct number!"
+  else
+    "Goodbye!"
+  end
 end
